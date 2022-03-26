c238492caebeaba3c919e301da4ba0acd8099d75
Joel <jlandaveri1@gmail.com>
2022-03-26 15:03:42 -0500

    Avance

--- src/Reporte.py
+++ src/Reporte.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import pandas_profiling
+import pandas_profiling as ppr
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
